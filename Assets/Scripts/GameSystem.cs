7db4f58f6c4f27cef1bccce5f5656753bc3e5ff1
klcatha <klcatha0518@gmail.com>
2018-10-31 15:30:45 +0900

    add update player image scale method

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -29,6 +29,30 @@ public class GameSystem : MonoBehaviour {
 	//デフォルトはモンスター
 	PlayerType playerType = PlayerType.Monster;
 
+	bool isFlipPlayerImage;
+	public bool IsFlipPlayerImage
+	{
+		set
+		{
+			isFlipPlayerImage = value;
+			UpdatePlayerImageScale();
+		}
+	}
+
+	float playerImageScale = 1.0f;
+	public void ChangePlayerImageScale(float scale)
+	{
+		playerImageScale = scale;
+		UpdatePlayerImageScale();
+	}
+
+	void UpdatePlayerImageScale()
+	{
+		Vector3 scale = Vector3.one * (playerImageScale * 0.4f + 0.6f);
+		if (isFlipPlayerImage) scale.x *= -1.0f;
+		playerImage.transform.localScale = scale;
+	}
+
 	public void Back()
 	{
 		Debug.Log("Back");
