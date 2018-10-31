58d95001ffbc7a3fc956b0f145dc4f51c34eead1
klcatha <klcatha0518@gmail.com>
2018-10-31 14:49:39 +0900

    add input field SerializeField

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour {
+
+	[SerializeField]
+	private InputField nameField;
 
 	public void Back()
 	{
